87d6a45a169db42f64fadd7b27e5956083207dc6
Lee Davies <lee@redtangle.co.uk>
2013-10-03 22:07:22 +0100

    Exercise 3
    Math and floating point lols

---
+++ ex3.py
@@ -0,0 +1,29 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# Floating point numbers
+print "This is a floating point: ", 20.25 * 5.21
+print "Is this a floating point?", 5 / 2
+# Typing the below directly in to python produces
+# 0.30000000000000004
+print "This may seem weird", 0.1 + 0.2
